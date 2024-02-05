5b86454a5a766b11ef7d495528c44f4c2d4b61c9
AndaluciaAO <andaluciaao@andaluciaao.com>
2024-02-05 18:34:18 +0100

    SCV - GIT - Taller 1 v1

---
+++ andaluciaao-scv-git-taller1/src/com/ndao/Saludos.java
@@ -0,0 +1,21 @@
+package com.ndao;
+
+/**
+ * Formación AndalucíaAO
+ */
+public class Saludos {
+
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hola Alejandro");
+		System.out.println("Hola Joaquín");
+		// TODO continúa el código con tu nombre de pila.
+
+	}
+
+}
